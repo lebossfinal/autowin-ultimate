69cd9630acd1bb6b58d73706b9083c0ee9ebb43e
lebossfinal <letrolldu18@hotmail.com>
2025-09-28 21:14:48 -0400

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../back/build'
+    outDir: '../back/build',
+    //outDir: '../build',
+    chunkSizeWarningLimit: 1000 // Sets limit to 1000 kB
+
   }
 })
 
